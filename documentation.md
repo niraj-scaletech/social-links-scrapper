431393eac9be8b9ddef221d08eaa304c35f97c0f
niraj-scaletech <niraj.visavadiya@scaletech.xyz>
2024-11-06 12:47:20 +0530

    update code

--- documentation.md
+++ documentation.md
@@ -1,5 +1,53 @@
-### Google search links scrapper
+### Social media profile statics scrapper
+
+## Overview
+
+The Social media profile statics scrapper is a powerful tool designed to automate the extraction of comprehensive statistics and information form social media account (Instagram,Facebook,Twitter). This tool crawls profiles to gather key metrics, including follower count, following count, and post activity, giving you a streamlined snapshot of social engagement.
+
+---
+
+## **Extracted Data Fields**
+
+| **Field**             | **Description**                                                   |
+|-----------------------|-------------------------------------------------------------------|
+| **Posts**             | The number of posts published by the user on the platform.        |
+| **Followers**         | The number of followers the user has on the platform.             |
+| **Following**         | The number of accounts the user is following on the platform      |
+| **Platform**          | The name of the social media platform (e.g., Instagram, Twitter). |
+
+---
+
+## Input Parameters
+
+- **Keywords**:  
+A list of URLs (one per line) that link to user profiles on social media platforms. Each URL is used to retrieve user-related data (e.g., posts, followers, following).
+  **Example**:  
+  ```
+    https://www.instagram.com/jl_struff/
+    https://www.facebook.com/jlstruff/
+    https://twitter.com/struffitennis/
+  ```
+
+## How to Use
+
+1. **Step 1**: Click `Try out!`
+2. **Step 2**: Enter the profile link.
+3. **Step 3**: Click the `Submit` button to run the sequence and extract data.
 
 
-Basic google search link scrapper
+## Sample Data Preview
 
+| **Posts** | **Followers** | **Following** | **Platform**  |
+|-----------|---------------|---------------|---------------|
+| 245       | 72.1K         | 558           | Instagram     |
+| N/A       | 17K           | 18            | Facebook      |
+| 312       | 113           | 8,156         | Twitter       |
+
+
+## Notes
+
+Please contact our support team if you have any questions about this template or if you would like to have a customized version.
+
+## Is Scraping social links Legal?
+
+Web scraping is typically allowed when gathering publicly available, non-personal data. However, how you use that data may have legal implications. Content like documentation, help articles, or blogs is often copyrighted, meaning you cannot republish it without permission from the owner. For more information on the legal aspects of web scraping, consider reviewing relevant resources or consulting with a legal professional if you're uncertain.
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Page } from "puppeteer-extra-plugin/dist/puppeteer";
 import { apikey, sequence_id, showBrowser } from "./config";
 import { browser, wait } from "@crawlora/browser";
 import debug from "debug";
+type NonNegativeInteger<T extends number> = number extends T ? never : `${T}` extends `-${string}` | `${string}.${string}` ? never : T;
 
 export default async function ({ searches }: { searches: string }) {
   const formedData = searches
@@ -15,27 +16,47 @@ export default async function ({ searches }: { searches: string }) {
         const platform = getPlatform(search);
         debug(`Platform: ${platform}`);
 
-        await page.goto(search, {
-          waitUntil: ["networkidle2"],
-          timeout: 60000,
-        });
-        debug(`Visiting ${platform}: ${search}`);
-        await wait(2);
+        try {
+          await page.goto(search, {
+            waitUntil: ["networkidle2"],
+            timeout: 60000,
+          });
+          debug(`Visiting ${platform}: ${search}`);
+          await wait(2);
 
-        let data;
-        switch (platform) {
-          case "Instagram":
-            data = await scrapeInstagram(page, wait, debug);
-            break;
-          case "Facebook":
-            data = await scrapeFacebook(page, wait, debug);
-            break;
-          case "Twitter":
-            data = await scrapeTwitter(page, debug);
-            break;
-          default:
-            debug(`Unsupported platform: ${search}`);
-            data = { error: "Unsupported platform" };
+          let data;
+          switch (platform) {
+            case "Instagram":
+              data = await scrapeInstagram(page, wait, debug);
+              break;
+            case "Facebook":
+              data = await scrapeFacebook(page, wait, debug);
+              break;
+            case "Twitter":
+              data = await scrapeTwitter(page, debug);
+              break;
+            default:
+              debug(`Unsupported platform: ${search}`);
+              data = {
+                posts: "N/A",
+                followers: "N/A",
+                following: "N/A",
+              };
+          }
+
+          await output.create({
+            sequence_id,
+            sequence_output: {
+              Posts: data.posts,
+              Followers: data.followers,
+              Following: data.following,
+              Platform: platform,
+              ProfileUrl: search
+            },
+          });
+        } catch (error) {
+          const e = error as Error;
+          debug(`Error scraping ${platform}:`, e.message);
         }
       },
       { showBrowser, apikey }
@@ -50,74 +71,98 @@ export function getPlatform(url: string) {
   return "Unknown";
 }
 
-async function scrapeInstagram(page: Page, wait: any, debug: debug.Debugger) {
-  const closeButton = await page.$(
-    'div[role="dialog"] svg[aria-label="Close"]'
-  );
-  if (closeButton) {
-    debug("Dialog detected in instagram");
-    closeButton.click();
-    await wait(1);
-    debug("Dialog closed with the close button.");
-  }
+async function scrapeInstagram(page: Page, wait: <N extends number>(sec: NonNegativeInteger<N>) => Promise<void>, debug: debug.Debugger) {
+  try {
+    const closeButton = await page.$('div[role="dialog"] svg[aria-label="Close"]');
+    if (closeButton) {
+      debug("Instagram dialog detected");
+      await closeButton.click();
+      await wait(1);
+      debug("Instagram dialog closed.");
+    }
 
-  return await page.evaluate(() => {
-    const [posts, followers, following] = Array.from(
-      document.querySelectorAll("li.xl565be .x5n08af")
-    ).map((el) => el.textContent?.trim());
+    return await page.evaluate(() => {
+      const [posts, followers, following] = Array.from(
+        document.querySelectorAll("li.xl565be .x5n08af")
+      ).map((el) => el.textContent?.trim());
+
+      return {
+        posts: posts || "N/A",
+        followers: followers || "N/A",
+        following: following || "N/A",
+      };
+    });
+  } catch (error) {
+    const e = error as Error;
+    debug("Instagram scraping error:", e.message);
     return {
-      posts,
-      followers,
-      following,
+      posts: "N/A",
+      followers: "N/A",
+      following: "N/A",
     };
-  });
+  }
 }
 
-// Scraper function for Facebook
-async function scrapeFacebook(page: Page, wait: any, debug: debug.Debugger) {
-  const closeButton = await page.$('div[aria-label="Close"]');
-  if (closeButton) {
-    debug("Dialog detected in facebook");
-    closeButton.click();
-    await wait(1);
-    debug("Dialog closed with the close button.");
-  }
+async function scrapeFacebook(page: Page, wait: <N extends number>(sec: NonNegativeInteger<N>) => Promise<void>, debug: debug.Debugger) {
+  try {
+    const closeButton = await page.$('div[aria-label="Close"]');
+    if (closeButton) {
+      debug("Facebook dialog detected");
+      await closeButton.click();
+      await wait(1);
+      debug("Facebook dialog closed.");
+    }
 
-  return await page.evaluate(() => {
-    const objElement = document.querySelector("div.xvrxa7q");
+    return await page.evaluate(() => {
+      const objElement = document.querySelector("div.xvrxa7q");
+      const elements = objElement?.querySelectorAll('a[role="link"]') || [];
 
-    const element = objElement?.querySelectorAll('a[role="link"]') || [];
+      return {
+        posts: "N/A",
+        followers: elements[1]?.textContent?.replace("followers", "").trim() || "N/A",
+        following: elements[2]?.textContent?.replace("following", "").trim() || "N/A",
+      };
+    });
+  } catch (error) {
+    const e = error as Error;
+    debug("Facebook scraping error:", e.message);
     return {
-      followers: element[1].textContent?.replace("followers", "").trim(),
-      following: element[2].textContent?.replace("following", "").trim(),
+      posts: "N/A",
+      followers: "N/A",
+      following: "N/A",
     };
-  });
+  }
 }
 
 async function scrapeTwitter(page: Page, debug: debug.Debugger) {
-  return await page.evaluate(() => {
-    const element = Array.from(document.querySelectorAll('a[role="link"]'));
-    const postElement = Array.from(
-      document.querySelectorAll("div.css-146c3p1")
-    );
+  try {
+    return await page.evaluate(() => {
+      const elements = Array.from(document.querySelectorAll('a[role="link"]'));
+      const postElement = Array.from(document.querySelectorAll("div.css-146c3p1"));
 
-    const following = element
-      .find((el) => el?.textContent?.includes("Following"))
-      ?.textContent?.replace("Following", "")
-      .trim();
-    const followers = element
-      .find((el) => el?.textContent?.includes("Followers"))
-      ?.textContent?.replace("Followers", "")
-      .trim();
-    const posts = postElement
-      .find((el) => el?.textContent?.includes("posts"))
-      ?.textContent?.replace("posts", "")
-      .trim();
+      const following = elements
+        .find((el) => el?.textContent?.includes("Following"))
+        ?.textContent?.replace("Following", "").trim() || "N/A";
+      const followers = elements
+        .find((el) => el?.textContent?.includes("Followers"))
+        ?.textContent?.replace("Followers", "").trim() || "N/A";
+      const posts = postElement
+        .find((el) => el?.textContent?.includes("posts"))
+        ?.textContent?.replace("posts", "").trim() || "N/A";
 
+      return {
+        posts,
+        following,
+        followers,
+      };
+    });
+  } catch (error) {
+    const e = error as Error;
+    debug("Twitter scraping error:", e.message);
     return {
-      posts,
-      following,
-      followers,
+      posts: "N/A",
+      followers: "N/A",
+      following: "N/A",
     };
-  });
+  }
 }
